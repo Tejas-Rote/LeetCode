dcf495fe1096dbb7a9eafbcc4bdc7c86ecdf0d56
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-01-06 22:34:15 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        //Your code here
+        string s="";
+        Node *t = head;
+        int num =0;
+        while(t != NULL){
+             num = num*10 + t->data;
+           
+            t =t->next;
+        }
+        
+        // cout<<num<<endl;
+        int rnum =0;
+        int temp = num;
+        while(temp>0){
+            int z = temp%10;
+            rnum = rnum*10 + z;
+            temp = temp/10;
+            // cout<<temp;
+            
+        }
+        
+        // cout<<rnum<<endl;
+        
+        if(rnum == num){
+            return true;
+        }
+        return false;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
