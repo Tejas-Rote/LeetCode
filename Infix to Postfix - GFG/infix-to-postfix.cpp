047fcafe189bf939cf01f0473346414731bfe2f9
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-01-18 23:16:39 +0530

    Added solution - LeetHub

---
+++ Infix to Postfix - GFG/infix-to-postfix.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+// C++ implementation to convert infix expression to postfix
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+    private: 
+        int prec(char c){
+            if(c == '^'){
+                return 3;
+            }else if(c == '*' || c=='/'){
+                return 2;
+            }else if(c == '+' || c== '-'){
+                return 1 ;
+            }else if (c == ')' || c == '('){
+                return 0;
+            }else return -1;
+        }
+  public:
+    // Function to convert an infix expression to a postfix expression.
+    string infixToPostfix(string s) {
+        // Your code here
+        
+        stack<char>st;
+        string ans ="";
+        for(int i=0;i<s.size();i++){
+            char c = s[i];
+            if(c >='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9' ){
+                ans += c;
+            }
+            else if(c == '('){
+                st.push(c);
+            }else if( c == ')'){
+                while(st.top() != '('){
+                    char z = st.top();
+                    ans+=z;
+                    st.pop();
+                }
+                st.pop();
+            }
+            else {
+                 while (!st.empty() && prec(s[i]) <= prec(st.top())) {
+                    ans += st.top();
+                    st.pop();
+                }
+                st.push(c);
+            
+                // if(st.empty()){
+                //     st.push(c);
+                // }else{
+                //     if(prec(c) > prec(st.top())){
+                //         st.push(c);
+                //     }else if(prec(c)< prec(st.top())){
+                        
+                //     }
+                // }
+            }
+        }
+        
+    while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+     
+    return ans;       
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(INT_MAX, '\n');
+    while (t--) {
+        string exp;
+        cin >> exp;
+        Solution ob;
+        cout << ob.infixToPostfix(exp) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
