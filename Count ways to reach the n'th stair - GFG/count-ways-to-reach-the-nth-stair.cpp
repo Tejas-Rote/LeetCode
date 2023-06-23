9de804820b85174603a429214c89d0664514592c
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-06-23 19:36:00 +0530

    Added solution - LeetHub

--- Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
@@ -27,15 +27,7 @@ class Solution
     //     // }
     // }
     
-    int solve(int n, vector<int>&dp){
-        
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]%mod+dp[i-2]%mod;
-        }
-        return (dp[n]%mod);
-    }
+
     
     
     int countWays(int n)
