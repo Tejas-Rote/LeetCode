4927bd3d3103ca59ce94bf461559a42d262f9ca5
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-07-18 15:28:15 +0530

    Time: 137 ms (41.13%), Space: 18.6 MB (44.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -31,27 +31,29 @@ public:
             // int res = helper(n-1 , amount, coins, dp);
             // return res == 1e9 ? -1 : res;
             
-            vector<vector<int>> dp (n, vector<int> (amount+1 ,0));
+            // vector<vector<int>> dp (n, vector<int> (amount+1 ,0));
+            vector<int> prev(amount+1, 0), cur(amount+1, 0);
             //base case 
             for(int i=0; i<=amount; i++){
                 if(i%coins[0] == 0)  
-                    dp[0][i] = i/coins[0];
-                else dp[0][i] = 1e9;
+                    prev[i] = i/coins[0];
+                else prev[i] = 1e9;
             }
             
              for(int ind = 1; ind<n; ind++){
                 for(int target = 0; target<=amount; target++){
 
-                    int notTake = 0 + dp[ind-1][target];
+                    int notTake = 0 +prev[target];
                     int take = 1e9;
                     if(coins[ind]<=target)
-                        take = 1 + dp[ind][target - coins[ind]];
+                        take = 1 + cur[target - coins[ind]];
 
-                     dp[ind][target] = min(notTake, take);
+                     cur[target] = min(notTake, take);
                 }
+                 prev = cur;
             }
 
-            int ans = dp[n-1][amount];
+            int ans = prev[amount];
             if(ans >=1e9) return -1;
             return ans;
             
