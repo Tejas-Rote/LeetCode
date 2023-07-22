e1a5c6424493c15380920ca1d3018c31241321f4
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-07-22 21:05:32 +0530

    Time: 74 ms (63.83%), Space: 6.7 MB (96.59%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -7,24 +7,26 @@ public:
         reverse(s2.begin(), s2.end());
         // cout<<s2<<endl;
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i] = 0;
-        }
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<int> prev(n+1, 0), cur(n+1, 0);
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = 0;
+        // }
+        // for(int i=0;i<=m;i++){
+        //     dp[0][i] = 0;
+        // }
 
         for(int ind1=1;ind1<=n;ind1++){
             for(int ind2=1;ind2<=m;ind2++){
                 if(s1[ind1-1]==s2[ind2-1])
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    cur[ind2] = 1 + prev[ind2-1];
                 else
-                    dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
             }
+            prev = cur;
         }
 
-        int l=  dp[n][m];
+        int l=  prev[m];
         // cout<<l<<endl;
         int res; 
         res = n- l;
