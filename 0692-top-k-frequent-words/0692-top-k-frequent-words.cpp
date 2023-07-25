35993983ebf0a4d49d37a78ea420cdf5116690bd
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-07-26 00:16:58 +0530

    Time: 18 ms (25.79%), Space: 12.5 MB (81.14%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,12 +1,22 @@
 class Solution {
-public:
-    static bool cmp(pair<int, string> a, pair<int, string> b){
-        if(a.first == b.first){
-            return a.second<b.second;
-        }else {
-            return a.first>b.first;
+private: 
+    struct Cmp {
+        bool operator()(const pair<int, string>& a, const pair<int, string>& b) const {
+            if (a.first == b.first) {
+                return a.second < b.second;
+            } else {
+                return a.first > b.first;
+            }
         }
-    }
+    };
+public:
+    // static bool cmp(pair<int, string> a, pair<int, string> b){
+    //     if(a.first == b.first){
+    //         return a.second<b.second;
+    //     }else {
+    //         return a.first>b.first;
+    //     }
+    // }
     vector<string> topKFrequent(vector<string>& words, int k) {
         int n = words.size();
         map<string, int> mp;
@@ -14,22 +24,32 @@ public:
         for(int i=0;i<n;i++){
             mp[words[i]]++;
         }
-        vector<pair<int, string>> v;
-        for(auto it: mp){
-            v.push_back({it.second,it.first});
-            // cout<<it.first<<" "<<it.second<<endl;
-        }
         
-        sort(v.begin() , v.end(), cmp);
-        // reverse(v.begin() , v.end());
-        for(auto i:v){
-            // cout<<i.first<<" "<<i.second<<endl; 
-            if(k>0){
-                ans.push_back(i.second);
-                k--;
-            }
+        priority_queue<pair<int, string>, vector<pair<int, string>>, Cmp> pq;
+        for(auto it = mp.begin(); it != mp.end(); ++it) {
+            pq.push(make_pair(it->second, it->first));
+            if(pq.size() > k) pq.pop();
         }
-        // sort(ans.begin(), ans.end());
+
+        while(!pq.empty()) {
+            ans.insert(ans.begin(), pq.top().second);
+            pq.pop();
+        }
         return ans;
+        
+        
+//         vector<pair<int, string>> v;
+//         for(auto it: mp){
+//             v.push_back({it.second,it.first});
+//         }
+        
+//         sort(v.begin() , v.end(), cmp);
+//         for(auto i:v){
+//             if(k>0){
+//                 ans.push_back(i.second);
+//                 k--;
+//             }
+//         }
+        // return ans;
     }
 };
