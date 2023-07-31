ebabe1983efda5ca21ea71ee1702bd9553e0fd8f
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-07-31 22:43:55 +0530

    Time: 3 ms (72.45%), Space: 11.9 MB (59.02%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -41,9 +41,21 @@ vector<int> ans;
             }
         }
     }
+    
+    void helper(TreeNode * root, int level , vector<int> &ans){
+        if(root==NULL){
+            return ;
+        }
+        if(ans.size() == level) ans.push_back(root->val);
+        
+        helper(root->right, level+1, ans);
+        helper(root->left, level+1, ans);
+    }
 
     vector<int> rightSideView(TreeNode* root) {
-        printf(root);
+        // printf(root);
+        vector<int> ans;
+        helper(root, 0, ans);
         return ans;
     }
 };
