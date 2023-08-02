a081b3d3787d10e815c485d7f6f6be570acd16ae
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-02 19:23:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.95%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -20,14 +20,26 @@ public:
     int climbStairs(int n) {
         // vector<int> dp(n+1, -1);
         // return helper(0, n, dp);
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
+        // vector<int> dp(n + 1, 0);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        
+        int curr ;
+        int prev1 =1;
+        int prev2 =1;
 
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            // dp[i] = dp[i - 1] + dp[i - 2];
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        // return dp[n];
+        return curr;
     }
 };
