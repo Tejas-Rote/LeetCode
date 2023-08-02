7f1f884de0615dd202a9f80a5ec5c794a1593b25
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-02 18:31:47 +0530

    Time: 4 ms (65.81%), Space: 13.9 MB (34.95%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void helper(int open , int close, int n , string s , vector<string> &ans){
+    vector<string> ans;
+    void helper(int open , int close, int n , string s){
         if(s.size()== (2*n)){
             ans.push_back(s);
             // s="";
             return ;
         }
         if(open<n){
-            helper(open+1, close, n , s+"(", ans);
+            helper(open+1, close, n , s+"(");
         }
         if(close<open){
-            helper(open , close+1, n, s+")", ans);
+            helper(open , close+1, n, s+")");
         }
         
     }
     
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
         string s;
-        helper(0,0, n,s, ans);
+        helper(0,0, n,s);
         return ans;
         
     }
