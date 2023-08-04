410217f141ac570ead837c050b7ea86ebb4bf3b6
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-04 16:59:50 +0530

    Time: 111 ms (88.46%), Space: 83.8 MB (33.32%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int> mp; //map of <gain,index> form
+        mp[0]=-1; //add starting index with default gain of 0 at -1
+        //change all zeros to -1
+        for(int i=0;i<nums.size();i++){
+            nums[i]==0?nums[i]=-1:nums[i]=1;
+        }
+        int sum=0,res=0;
+        for(int i=0;i<nums.size();i++){
+            //cumulative sum
+            sum+=nums[i];
+            // cout<<"sum "<<sum<<endl;
+            //check if value already exists in the map
+            if(mp.find(sum)!=mp.end()){
+                //finding the length of possible subarray and comparing with the max result
+                res=max(res,i-mp[sum]);
+                // cout<<res<<" i "<<i<<endl;
+            }
+            else{
+                //adding value to our map
+                mp[sum]=i;
+            }
+        }
+            // cout<<endl;
+        return res;
+    }
+};
