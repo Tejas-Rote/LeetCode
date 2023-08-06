8d1c800283d3fbd73faafb5548f040eb0d0f4556
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-06 22:45:00 +0530

    Time: 523 ms (40.32%), Space: 292.3 MB (15.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -16,7 +16,29 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        return helper(0, -1, nums, dp);
+        // vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        // return helper(0, -1, nums, dp);
+        //base case
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+        for(int ind = n-1; ind>=0; ind --){
+            for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+
+                int notTake = 0 + dp[ind+1][prev_index +1];
+
+                int take = 0;
+
+                if(prev_index == -1 || nums[ind] > nums[prev_index]){
+
+                    take = 1 + dp[ind+1][ind+1];
+                }
+
+                dp[ind][prev_index+1] = max(notTake,take);
+
+            }
+        }
+
+        return dp[0][0];
     }
 };
