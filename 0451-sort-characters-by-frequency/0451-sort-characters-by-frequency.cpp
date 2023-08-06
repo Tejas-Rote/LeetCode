3e6404d52f1bcd817f867a3c53844652e4b04355
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-07 00:01:49 +0530

    Time: 15 ms (34.33%), Space: 8.6 MB (17.62%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,41 +1,55 @@
 class Solution {
 public:
-    static bool cmp(pair<char, pair<int, int>> a, pair<char, pair<int, int>> b) {
-        if (a.second.first != b.second.first) {
-            return a.second.first > b.second.first; // Sort by frequency in descending order
-        }
-        return a.second.second < b.second.second; // Sort by index in ascending order
-    }
+    // static bool cmp(pair<char, pair<int, int>> a, pair<char, pair<int, int>> b) {
+    //     if (a.second.first != b.second.first) {
+    //         return a.second.first > b.second.first; 
+    //     }
+    //     return a.second.second < b.second.second; 
+    // }
     string frequencySort(string s) {
-        int n = s.size();
-        map<char, pair<int, int>> mp;
-        for(int i=0;i<n;i++){
-            if(mp.find(s[i] )== mp.end()){
-                mp.insert({s[i], {1, i}});
-            }else {
-                mp[s[i]].first++;
-            }
+        unordered_map<char, int> freqMap;
+        for (char c : s) {
+            freqMap[c]++;
         }
-        
-        // for(auto i :mp){
-        //     cout<<i.first<<" -> "<<i.second.first<<" "<<i.second.second<<endl;
-        // }
-        // cout<<endl;
-        vector<pair<char, pair<int, int>>> vec;
-        for (auto it : mp) {
-            vec.push_back(it);
+
+        priority_queue<pair<int, char>> pq;
+        for (auto it : freqMap) {
+            pq.push({it.second, it.first});
         }
-        sort(vec.begin(), vec.end(), cmp);
-        
+
         string ans;
-        for (auto it : vec) {
-            // cout << "Char: " << it.first << ", f: " << it.second.first << ", i : " << it.second.second << endl;
-            while(it.second.first--){
-                ans +=it.first;
+        while (!pq.empty()) {
+            pair<int, char> topElement = pq.top();
+            pq.pop();
+            while (topElement.first--) {
+                ans += topElement.second;
             }
         }
-        
-        // reverse(ans.begin(), ans.end());
+
         return ans;
+//         int n = s.size();
+//         map<char, pair<int, int>> mp;
+//         for(int i=0;i<n;i++){
+//             if(mp.find(s[i] )== mp.end()){
+//                 mp.insert({s[i], {1, i}});
+//             }else {
+//                 mp[s[i]].first++;
+//             }
+//         }
+
+//         vector<pair<char, pair<int, int>>> vec;
+//         for (auto it : mp) {
+//             vec.push_back(it);
+//         }
+//         sort(vec.begin(), vec.end(), cmp);
+        
+//         string ans;
+//         for (auto it : vec) {
+//             while(it.second.first--){
+//                 ans +=it.first;
+//             }
+//         }
+
+//         return ans;
     }
 };
