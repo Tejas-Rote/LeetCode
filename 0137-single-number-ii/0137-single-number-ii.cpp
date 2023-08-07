d3e114f4f2ae823c604878f590c63beed4e2097a
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-07 23:56:17 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (65.75%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int ans;
+        int n = nums.size();
+        if(n==1){
+            return nums[0];
         }
-        int ans = 0;
-        for(auto i: mp){
-            if(i.second == 1){
-                ans = i.first;
+        sort(nums.begin(), nums.end());
+
+        if (nums[1] != nums[0]) {
+            return nums[0];
+        }
+
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                ans = nums[i];
                 break;
             }
         }
+        // unordered_map<int, int> mp;
+        // for(int i=0;i<nums.size();i++){
+        //     mp[nums[i]]++;
+        // }
+        // int ans = 0;
+        // for(auto i: mp){
+        //     if(i.second == 1){
+        //         ans = i.first;
+        //         break;
+        //     }
+        // }
         return ans;
     }
 };
