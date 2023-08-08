c73e8d679cb6da19b0f60a9c9d8b233401ca5e5d
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-08 23:47:53 +0530

    Time: 3 ms (40.74%), Space: 7 MB (32.46%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        int delrow[] = {-1,-1,-1,0,1,1,1,0};
-        int delcol[] = {-1,0,1,1,1,0,-1,-1};
+        int delrow[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+        int delcol[] = {-1, 0, 1, 1, 1, 0, -1, -1};
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>> ans(n, vector<int> (m, 0));
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int ln =0;
-                for(int k=0;k<8;k++){
-                    int nrow = i+delrow[k];
-                    int ncol = j+delcol[k];
-                    if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && board[nrow][ncol] == 1){
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int ln = 0;
+                for(int k = 0; k < 8; k++) {
+                    int nrow = i + delrow[k];
+                    int ncol = j + delcol[k];
+                    if(nrow < n && nrow >= 0 && ncol < m && ncol >= 0 && (board[nrow][ncol] == 1 || board[nrow][ncol] == 3)) {
                         ln++;
                     }
                 }
-                cout<<ln<<" " ;
-                
-                if(board[i][j] == 1){
-                    if(ln<2){
+
+                if(board[i][j] == 1) {
+                    if(ln < 2 || ln > 3) {
                         ans[i][j] = 0;
                     }
-                    if(ln == 2 || ln == 3){
+                    else {
                         ans[i][j] = 1;
                     }
-                    if(ln>3){
-                        ans[i][j] = 0;
-                    }
-                }else {
-                    if(ln == 3){
+                }
+                else {
+                    if(ln == 3) {
                         ans[i][j] = 1;
                     }
                 }
             }
-            cout<<endl;
         }
-        board = ans;
-        
+
+        // Update the original board with the values from ans
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                board[i][j] = ans[i][j];
+            }
+        }
     }
 };
