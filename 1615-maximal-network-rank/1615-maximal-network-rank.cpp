948fd422bff771b07f6498d4bbcfd8e807400f91
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-18 21:52:51 +0530

    Time: 51 ms (98.50%), Space: 29.9 MB (89.51%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        int ans;
         vector<vector<int>> adj(n, vector<int> (n, 0));
+        vector<int> degree(n);
         for(int i=0;i<roads.size();i++){
             adj[roads[i][0]][roads[i][1]] = 1;
             adj[roads[i][1]][roads[i][0]] = 1;
         }
         
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<adj[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        // cout<<endl;
-        
-        vector<int> degree(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(adj[i][j] == 1){
@@ -24,10 +15,6 @@ public:
                 }
             }
         }
-        // for(int i=0;i<n;i++){
-        //     cout<<degree[i]<<" ";
-        // }
-        // cout<<endl;
         
         int maxi = INT_MIN;
         for(int i=0;i<n;i++){
@@ -37,14 +24,11 @@ public:
                     if(adj[i][j] == 1){
                         val -=1;
                     }
-                    // cout<<val<<" ";
                     maxi = max(maxi, val);
                 }
             }
-            // cout<<endl;
         }
         
-        ans = maxi;
-        return ans;
+        return maxi;
     }
 };
