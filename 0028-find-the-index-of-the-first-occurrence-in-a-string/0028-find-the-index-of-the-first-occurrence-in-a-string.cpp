131ac088c1baf355e4b7f44057e5a412c3c1f168
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-19 12:06:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (24.53%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
     int strStr(string a, string b) {
-        int index = a.find(b);
-        if(index != string::npos) return index;
-        else return -1;
+        // int index = a.find(b);
+        // if(index != string::npos) return index;
+        // else return -1;
+        
+        
+        int m = a.length();
+        int n = b.length();
+
+        if (m == 0 || n == 0 || m < n) return -1;  
+
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (a[i] == b[j]) {  
+                i++;
+                j++;
+                if (j == n) {  
+                    return i - n;
+                }
+            } else {  
+                i = i - j + 1;  
+                j = 0;  
+            }
+        }
+        return -1;          
     }
     
 };
