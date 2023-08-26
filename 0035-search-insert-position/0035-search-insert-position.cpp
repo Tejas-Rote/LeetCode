2a1ef1810530933e17c96cecde9f3c67df7d7746
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-26 22:30:39 +0530

    Time: 3 ms (80.77%), Space: 9.7 MB (53.81%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int searchInsert(vector<int>& a, int x) {
-        int l = 0;
-        int r = a.size()-1;
-        int mid;
-        while(l<=r){
-             mid = l + (r-l)/2;
+//         int l = 0;
+//         int r = a.size()-1;
+//         int mid;
+//         while(l<=r){
+//              mid = l + (r-l)/2;
             
-            if(a[mid] == x){
-                return mid;
-            }
+//             if(a[mid] == x){
+//                 return mid;
+//             }
             
-            if(a[mid] < x){
-                 l = mid +1;
-            }else {
-                r = mid-1;
-            }
-        }
+//             if(a[mid] < x){
+//                  l = mid +1;
+//             }else {
+//                 r = mid-1;
+//             }
+//         }
         
-        return l;
+//         return l;
+        int ind = lower_bound(a.begin(), a.end(), x) - a.begin();
+        return ind;
     }
 };
