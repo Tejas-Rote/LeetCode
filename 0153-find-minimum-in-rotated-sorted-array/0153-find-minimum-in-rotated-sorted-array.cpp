420eb1f9640debe87377b9379236807c7933a4e6
Tejas Ravindra Rote <76241687+Tejas-Rote@users.noreply.github.com>
2023-08-28 22:02:49 +0530

    Time: 3 ms (68.33%), Space: 10.2 MB (49.77%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,8 +5,6 @@ public:
         int l =0;
         int r = n-1;
         int mini = INT_MAX;
-     
-        
         while(l<=r){
             int mid = l + (r-l)/2;
             mini = min(mini, nums[mid]);
